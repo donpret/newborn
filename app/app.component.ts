f220d3bc9d4d88da2cfed7a46a1e05959664967c
raviragu <naravichandran@gmail.com>
2016-06-26 18:19:13 -0500

    Interactive modules with UI controls

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,6 +1,21 @@
 import { Component } from '@angular/core';
+import { Veggie } from './veggie';
 @Component({
   selector: 'my-app',
-  template: '<h1>My First Angular 2 App</h1>'
+  templateUrl: 'app/app.component.html'
 })
-export class AppComponent { }
+export class AppComponent {
+title = 'Newly Born Organic Neighbors!';
+welcomeMsg = 'Welcome to Organic Farms!';
+veggies = [new Veggie(1,'Carrot'),
+	new Veggie(2,'Beans'),
+	new Veggie(3, 'Spinach'),
+	new Veggie(4, 'Watermelon'),
+	new Veggie(5, 'Berries')];
+selectedVeggie = this.veggies[0];
+
+addVeggie(newVeggie:any){
+	this.veggies.push(new Veggie(this.veggies.length, newVeggie.value);
+	newVeggie.value = '';
+}
+}
---
+++ app/veggie.ts
@@ -0,0 +1,5 @@
+export class Veggie{
+	constructor(
+		public id: number,
+		public name: string){}
+}
